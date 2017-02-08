0f373087a3284a99c1132aa68a558a31b666ac7b
tontof <tontof@free.fr>
2017-02-08 16:03:43 +0100

    ADD: web-mode for html

--- setup/mode-mappings.el
+++ setup/mode-mappings.el
@@ -68,6 +68,19 @@ if you change this variable."
 	    (zencoding-mode 1)
 	    (define-key zencoding-mode-keymap (kbd "C-c C-j") 'zencoding-expand-line)))))))
 
+;; HTML
+(add-to-list 'auto-mode-alist '("\\.html" . sgml-mode))
+(eval-after-load "sgml-mode"
+  '(progn
+     ((lambda ()
+        (require 'multi-web-mode)
+        (setq mweb-default-major-mode 'html-mode)
+        (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                          (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+                          (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+        (setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+        (multi-web-global-mode 1)))))
+
 ;; JAVASCRIPT
 (autoload 'js3-mode "js3" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
